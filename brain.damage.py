7fbf69c504bbe67fe76c46702ecb59aa9e7d732e
Nikolaos Zisos <nikoszisos@gmail.com>
2026-03-15 20:22:53 +0200

    ok

--- brain.damage.py
+++ brain.damage.py
@@ -1,3 +1,5 @@
 my_list = [1, 2, 3]
-my_tuple = tuple(((my_list * 2)[1:5] + list((7, 8)))*4)
-print(my_tuple)
+my_new_list = ((my_list * 2)[1:5] + list((7, 8)))*4
+print("Η αρχική λίστα: " + str(my_list))
+print("Η λίστα με τις πράξεις: " + str(my_new_list))
+print("Πλήθος των 2 απο λίστα και λίστα με πράξεις μαζί: " + str((my_list+my_new_list).count(2)))
