cea64bf01147fa0d57d3c6b898b9870b47858535
Nikolaos Zisos <nikoszisos@gmail.com>
2026-03-15 20:07:31 +0200

    ok

---
+++ brain.damage.py
@@ -0,0 +1,3 @@
+my_list = [1, 2, 3]
+my_tuple = tuple(((my_list * 2)[1:5] + list((7, 8)))*4)
+print(my_tuple)
---
+++ sequence.ops.py
@@ -0,0 +1,7 @@
+numbers = (1, 2, 3, 4, 5, 3, 8, 3)
+print("Length: " + str(len(numbers)))
+print("Min: " + str(min(numbers)))
+print("Max: " + str(max(numbers)))
+print("Πλήθος των 3: " + str(numbers.count(3)))
+print("Θέση του πρώτου 3: " + str(numbers.index(3, 0)))
+
