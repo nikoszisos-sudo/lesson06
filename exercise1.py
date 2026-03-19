ca203f2f40c7df933d36ff5835c04ac1496a1f35
Nikolaos Zisos <nikoszisos@gmail.com>
2026-03-19 19:32:56 +0200

    ok

--- exercise1.py
+++ exercise1.py
@@ -1,9 +1,9 @@
 my_list = []
 print ("Δώσε 10 αριθμούς απο το 10 ως 20: ")
-for i in range(1, 11, 1):
-    x = int(input("Δώσε τον " + str(i) + "ο ακέραιο: "))
+for i in range(10):
+    x = int(input("Δώσε τον " + str(i+1) + "ο ακέραιο: "))
     while x < 10 or x > 20:
-        x = int(input("Δώσε τον " + str(i) + "ο ακέραιο: "))
+        x = int(input("Δώσε τον " + str(i+1) + "ο ακέραιο: "))
     my_list.append(x)
 print(my_list)
 my_tuple = tuple(my_list)
@@ -11,6 +11,8 @@ print(my_tuple)
 my_list.sort()
 my_tuple2 = tuple(my_list)
 print(my_tuple2)
-squared_list = [x**2 for x in my_tuple2]
+squared_list = []
+for x in range(10):
+    squared_list.append(my_list[x]**2)
 squared_tuple = tuple(squared_list)
 print(squared_tuple)
