3c6db221e095fc92948b9a5ee4b1beadfc08a89e
Nikolaos Zisos <nikoszisos@gmail.com>
2026-03-19 21:55:08 +0200

    ok

--- exercise3.py
+++ exercise3.py
@@ -2,7 +2,7 @@ N = int(input("Δώσε ένα πρώτο αριθμό: "))
 while N <= 1:
     N = int(input("Προσοχή. Πρέπει να είναι μεγαλύτερος απο το 1: "))
     for i in range(N):
-        if (N % N == 0 and N % 1 == 0 and N % 2 !=0 and N % (N-1) !=0) or (N == 2):
+        if (N % N == 0 and N % 1 == 0 and N % 2 !=0 and N % (i+1) !=0) or (N == 2):
             print("Σωστά! Είναι πρώτος.")
 else:
     print("Δεν είναι πρώτος.")
