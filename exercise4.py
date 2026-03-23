572e26e0af9a47ceb1e8a1286cc80e22d04b4eca
Nikolaos Zisos <nikoszisos@gmail.com>
2026-03-23 14:39:30 +0200

    ok

--- exercise4.py
+++ exercise4.py
@@ -1,8 +1,10 @@
 list1 = []
+tuple1 = ()
 for i in range(2, 101):
     for j in range(2, i):
         if i % j == 0:
             break
     else:
         list1.append(i)
-print(list1)
+tuple1 = tuple(list1)
+print(tuple1)
