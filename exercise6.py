16e26be6355b577e46b6d946e4b2a7f4b0539dce
Nikolaos Zisos <nikoszisos@gmail.com>
2026-03-24 22:13:25 +0200

    ok

--- exercise6.py
+++ exercise6.py
@@ -1,10 +1,19 @@
 array = [[1,2,3,4], [5,6,7,8], [9,10,11,12]]
 array.insert(0, [0,0,0,0])
-print(array)
-array.append([1,1,1,1])
-print(array)
 
 for row in array:
+    for element in row:
+        print(element, end=" ")
+    print()
+
+for row in array:
+    row.append(1)
+    for element in row:
+        print(element, end=" ")
+    print()
+
+for row in array:
+    row.insert(0, 99)
     for element in row:
         print(element, end=" ")
     print()
