aff0b6f563f313cf02faaee47d81ad9fc9771f54
Nikolaos Zisos <nikoszisos@gmail.com>
2026-03-30 19:40:29 +0300

    ok

--- exercise7.py
+++ exercise7.py
@@ -2,7 +2,7 @@ hidden_cards = [1,2,3,3,2,4,1,4]
 N = 8
 cards_state = ["closed", "closed", "closed", "closed", "closed", "closed", "closed", "closed"]
 
-#βαζω σ'ενα πινακα 8 αριθμους, μια μεταβλητη Ν=8 και ενα πινακα με 8 τιμες στρινγκ ολες με κατασταση κλειστη
+#βαζω μια λιστα με 8 αριθμους, μια μεταβλητη Ν=8 και μια λιστα με 8 τιμες στρινγκ ολες με κατασταση κλειστη
 
 active_game = True
 while active_game:
@@ -19,7 +19,6 @@ while active_game:
 # για τα ορια μεταξυ 0 και 7 + οχι κατασταση opened + οχι 2 ιδιες καρτες
 # οι καρτες θα γινουν απο closed σε temp opened.
 
-
     for position in range(N):
         if cards_state[position] == "closed":
             print("_", end=" ")
@@ -27,6 +26,17 @@ while active_game:
             print(hidden_cards[position], end=" ")
         else:
             print(hidden_cards[position], end=" ")
+
+#για ολες τις 8 θεσεις (Ν=8) αν η θεση που εξεταζω στη λιστα cards_state
+    if hidden_cards[card1] == hidden_cards[card2]:
+        cards_state[card1] = "opened"
+        cards_state[card2] = "opened"
+    else:
+        cards_state[card1] = "closed"
+        cards_state[card2] = "closed"
+
+
+
 
 
     active_game = False
